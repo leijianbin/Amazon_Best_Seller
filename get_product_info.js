0edff8217b2852a13ada10d906c2e4c25cfb16e8
Jianbin Lei <leijianljb@gmail.com>
2014-12-17 01:09:01 -0800

    fix the info.txt format

--- get_product_info.js
+++ get_product_info.js
@@ -21,52 +21,51 @@ var url = 'http://www.amazon.com/dp/' + asin;
 //console.log(url);
 var body = fs.readFileSync(product_directory + element,'utf8');
 var $ = cheerio.load(body);
-
-		//check if ban by the amazon, <title dir="ltr">Robot Check</title>
-		var robot_check = $('title').text();
-		if(robot_check == "Robot Check")
-		{
-			console.log("Error, Robot Check");
+//check if ban by the amazon, <title dir="ltr">Robot Check</title>
+var robot_check = $('title').text();
+if(robot_check == "Robot Check")
+{
+	console.log("Error, Robot Check");
+}
+else {	
+	var title = $('#productTitle')? $('#productTitle').text().replace('\n',' '):'N/A';
+	var review_text = $('#acrCustomerReviewText')?$('#acrCustomerReviewText').text().trim():'';
+	var review_num = 0;
+	if (review_text){
+		review_num = review_text.match(/\d+,\d+|\d+/)[0];
+		review_num = review_num.replace(',','');
+	}
+	var review_star = $('#acrPopover').length > 0 ? $('#acrPopover').attr('title'):0;
+	if (review_star){
+		review_star = review_star.match(/\d+.\d+|\d+/)[0];
+	}
+	var image_url = $('#landingImage').data('a-dynamic-image')? Object.keys($('#landingImage').data('a-dynamic-image'))[0]:'';
+	var max_price, min_price;
+	max_price = min_price = 0;
+	var price = $('#priceblock_ourprice')?$('#priceblock_ourprice').text():'';
+	var saleprice = $('#priceblock_saleprice')?$('#priceblock_saleprice').text():'';
+	var dealprice = $('#priceblock_dealprice span')?$('#priceblock_dealprice span').first().text():'';
+	if (price.length < 2){
+		price = saleprice;
+		if (price.length < 2){
+			price = dealprice;
 		}
-		else {	
-			var title = $('#productTitle')? $('#productTitle').text().replace('\n',' '):'N/A';
-			var review_text = $('#acrCustomerReviewText')?$('#acrCustomerReviewText').text().trim():'';
-			var review_num = 0;
-			if (review_text){
-				review_num = review_text.match(/\d+,\d+|\d+/)[0];
-				review_num = review_num.replace(',','');
-			}
-			var review_star = $('#acrPopover').length > 0 ? $('#acrPopover').attr('title'):0;
-			if (review_star){
-				review_star = review_star.match(/\d+.\d+|\d+/)[0];
-			}
-			var image_url = $('#landingImage').data('a-dynamic-image')? Object.keys($('#landingImage').data('a-dynamic-image'))[0]:'';
-			var max_price, min_price;
-			max_price = min_price = 0;
-			var price = $('#priceblock_ourprice')?$('#priceblock_ourprice').text():'';
-			var saleprice = $('#priceblock_saleprice')?$('#priceblock_saleprice').text():'';
-			var dealprice = $('#priceblock_dealprice span')?$('#priceblock_dealprice span').first().text():'';
-			if (price.length < 2){
-				price = saleprice;
-				if (price.length < 2){
-					price = dealprice;
-				}
-			}
-			if (price.length > 1){
-				var price_tmp = price.replace(/[^0-9.-]/g,'');
-				var price_array = price_tmp.split('-');
-				if (price_array.length > 1){
-					min_price = price_array[0];
-					max_price = price_array[1];
-				}else{
-					min_price = max_price = price_array[0];
-				}
-			}
-			var rank_div = $('#SalesRank span');
+	}
+	if (price.length > 1){
+		var price_tmp = price.replace(/[^0-9.-]/g,'');
+		var price_array = price_tmp.split('-');
+		if (price_array.length > 1){
+			min_price = price_array[0];
+			max_price = price_array[1];
+		}else{
+			min_price = max_price = price_array[0];
+		}
+	}
+	var rank_div = $('#SalesRank span');
 			//console.log(rank_div)
 			var rank = 'N/A';
 			var category = '';
-			if (rank_div.length != 0){ //There are some products without rank
+			if (rank_div.length != 0) { //There are some products without rank
 			//rank_div.children().remove();
 			var rank = $('#SalesRank span')[0]['children'][0]['data'];
 			rank = rank.replace('#','');
@@ -76,12 +75,12 @@ var $ = cheerio.load(body);
 				// rank = rank_text.match(/\d+,\d+|\d+|\d+,\d+,\d+/)[0];
 				// rank = rank.replace(',','');
 				var testcategory = $('#SalesRank span a')[0]['children'][0]['data'];
-				// console.log(testcategory);
-				//category = 'Electronics';
-			}else{
-				rank = 0;
-				category = 'Others';
-			}
+					// console.log(testcategory);
+					//category = 'Electronics';
+				} else {
+					rank = 0;
+					category = 'Others';
+				}
 			//console.log(category);
 			// .match(/\d+,\d+|\d+/)[0];
 		}
@@ -99,13 +98,13 @@ var $ = cheerio.load(body);
 			rank: rank,
 			category: category
 		};
-//		console.log(result);
-var data = asin + '\t' + url + '\t'+title + '\t' + image_url +'\t'+review_num + '\t'+review_star + '\t' + min_price + '\t' + max_price+ '\t'+rank +'\t'+category + '\t' + current_date;
-		//console.log(data);
-		console.log("Fetch info sucess!");
-		fs.writeFileSync(info_file, data, {flag:'a'});
-	}
-});
+			//console.log(result);
+			var data = asin + '\t' + url + '\t'+title + '\t' + image_url +'\t'+review_num + '\t'+review_star + '\t' + min_price + '\t' + max_price+ '\t'+rank +'\t'+category + '\t' + current_date + '\n';
+			//console.log(data);
+			console.log("Fetch info sucess!");
+			fs.writeFileSync(info_file, data, {flag:'a'});
+		}
+	});
 
 
 
